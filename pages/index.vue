54b06660aac1a538aa75611c13ee73e9d409572d
AC11DB <cadizangelicamael@gmail.com>
2024-01-19 00:08:05 -0700

    yo

--- pages/index.vue
+++ pages/index.vue
@@ -2,19 +2,63 @@
 const { data: hello } = await useFetch("/api/hello");
 const { data } = await useFetch("/api/disney");
 const characters = data;
-const clientSideExample = "Updated Blah";
+const clientSideExample = "Disney Characters";
 </script>
 
 <template>
   <main>
     <header>
-      <h1>Hello CPRG</h1>
+      <h1>Assignment 2 - Data Fetching</h1>
     </header>
 
     <section>
-      <h2>Hello Data Endpoint Example</h2>
-      <p>{{ hello.hello }}</p>
-      {{ clientSideExample }}
+      <h2>1. Nuxt Setup & Git Management
+      </h2>
+        <p>Correct scaffolding
+        git commits at least 2 (partial marks)<p>
+          Commits >= 5 (full marks)
+        </p></p>
+      <h2>2. Server Route Code
+      </h2>
+        <p>
+          Async fetch function
+        </p>
+        <p>
+          Nuxt server boiler plate code setup
+        </p>
+        <p>
+          Naming conventions for files
+        </p>
+        <p>
+          Data returned
+        </p>
+      <h2>3.Client Side Code
+      </h2>
+        <p>
+          UseFetch composable
+        </p>
+        <p>
+          Rendered on the page
+        </p>
+        <p>
+          Pre tags for partial marks
+        </p>
+        <p>
+          V-for loop with data binding for full marks
+        </p>
+      <h2>4. Deployment
+      </h2>
+      <p>
+        full deployment === full marks
+      </p>
+      <p>
+        broken deployment === 1/2 marks
+      </p>
+      <p>
+        no deployment === no marks
+      </p>
+      <h1>{{ clientSideExample }}
+      </h1>
     </section>
     <pre>{{ characters }}</pre>
     <section>
